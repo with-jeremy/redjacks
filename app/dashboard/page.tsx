14ed224e7f00109b8c65c122e47d352a872b5de1
with-jeremy <jeremy@withjeremy.com>
2025-02-22 04:46:30 -0600

    removed bugging users role management

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -8,18 +8,19 @@ const Dashboard = () => {
 
   return (
     <div className="grid gap-5 p-5 bg-gray-800">
-      <Link href="/dashboard/users">
-      <button
-        className="p-5 m-2 text-lg bg-gray-200 text-gray-800 w-40 h-40"
-      >
-        User Management
-      </button>
-      </Link>
       <Link href="/dashboard/events">
       <button className="p-5 m-2 text-lg bg-gray-200 text-gray-800 w-40 h-40">
         Events Management
       </button>
       </Link>
+      <Link href="/">
+      <button
+        className="p-5 m-2 text-lg bg-gray-200 text-gray-800 w-40 h-40"
+      >
+        Logout
+      </button>
+      </Link>
+      
     </div>
   );
 };
