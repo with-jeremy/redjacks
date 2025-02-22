1002e7f89ae1694459c78e9f3916fc241935a6a7
with-jeremy <jeremy@withjeremy.com>
2025-02-22 03:26:35 -0600

    removed more type errors

--- app/dashboard/users/_actions.ts
+++ app/dashboard/users/_actions.ts
@@ -3,7 +3,7 @@
 import { checkRole } from '@/utils/roles'
 import { clerkClient } from '@clerk/nextjs/server'
 
-export async function setRole(formData: FormData): Promise<void> {
+export async function setRole(formData: FormData): Promise<{ message: string }> {
   const client = await clerkClient()
 
   // Check that the user trying to set the role is an admin
@@ -15,21 +15,21 @@ export async function setRole(formData: FormData): Promise<void> {
     const res = await client.users.updateUserMetadata(formData.get('id') as string, {
       publicMetadata: { role: formData.get('role') },
     })
-    return { message: res.publicMetadata }
+    return { message: JSON.stringify(res.publicMetadata) }
   } catch (err) {
-    return { message: err }
+    return { message: String(err) }
   }
 }
 
-export async function removeRole(formData: FormData): Promise<void> {
+export async function removeRole(formData: FormData): Promise<{ message: string }> {
   const client = await clerkClient()
 
   try {
     const res = await client.users.updateUserMetadata(formData.get('id') as string, {
       publicMetadata: { role: null },
     })
-    return { message: res.publicMetadata }
+    return { message: JSON.stringify(res.publicMetadata) }
   } catch (err) {
-    return { message: err }
+    return { message: String(err) }
   }
 }
